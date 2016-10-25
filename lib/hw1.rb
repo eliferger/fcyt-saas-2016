3904e81c84173b04d39fe1274ab47f4ef03ae49f
elii.rferger <elii.rferger@gmail.com>
2016-10-25 19:20:55 -0400

    hw1 parte 1

--- lib/hw1.rb
+++ lib/hw1.rb
@@ -5,15 +5,24 @@ VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 # Part 1
 # http://ruby-doc.org/core-2.2.0/Regexp.html
 def validate_email?(email)
+  m=email=~VALID_EMAIL_REGEX
+  m!=nil
   # YOUR CODE HERE
 end
 
 def is_palindrome?(phrase)
+  if phrase.empty?
+     return false
+ else
+  w = phrase.downcase.scan(/\w/)
+  w==w.reverse
   # YOUR CODE HERE
-end
+  end
+ end
 
 # Part 2
 def max_sum_of_2(arr1, arr2)
+  
   # YOUR CODE HERE
 end
 
